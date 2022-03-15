241c4632af5399b7d8fa2ee617a8968551c48661
karimDevWM <67164906+karimDevWM@users.noreply.github.com>
2022-03-15 16:08:55 +0100

    Update ma_bibliotheque.php

--- ma_bibliotheque.php
+++ ma_bibliotheque.php
@@ -3,7 +3,6 @@
 // TODO: Corriger l'erreur dans la fonction
 function trouver_min($tableau) {
     $min = $tableau[0];
-
     foreach ($tableau as $element) {
         if ($element<=$min) {
             $min = $element;
@@ -29,7 +28,33 @@ function trouver_max($tableau) {
 // Fonction pour trouver la valeur la plus proche de zéro
 function trouver_plus_proche_de_zero($tableau) {
     // TODO: Écrire le corps de la fonction
+    $procheZero = $tableau[0];
+    foreach ($tableau as $element) {
+        if (abs($element)<abs($procheZero)) {
+            $procheZero = $element;
+        }
+    }
+
+    return $procheZero;
 }
 
 // TODO: Ajouter les fonctions manquantes
+function productNumber($number)
+{
+    $result = $number*$number;
+    if(($number * $number)==$result)
+    {
+        return $result;
+    }
+}
+
+function matchAlphabetIndex($index)
+{
+    $alphaTable = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                    's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+    return $alphaTable[$index-1];
+}
+
 ?>
